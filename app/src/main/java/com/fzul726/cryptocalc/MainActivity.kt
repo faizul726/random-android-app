811a7c4c4d3673b6a5cb36f3dd8c83f96d35e567
Ekta Fazil <162413089+faizul726@users.noreply.github.com>
2024-07-28 19:56:02 +0600

    4th

---
+++ app/src/main/java/com/fzul726/cryptocalc/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.fzul726.cryptocalc
+
+import android.os.Bundle
+import android.webkit.WebView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val webView: WebView = findViewById(R.id.webView)
+        webView.settings.javaScriptEnabled = true
+        webView.loadUrl("file:///android_asset/index.html")
+    }
+}
